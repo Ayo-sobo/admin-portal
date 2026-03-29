ad9757f0b75159d80dccb2667080aaba969806ee
Ayo-sobo <hemmacenation@gmail.com>
2026-03-29 12:46:29 +0100

    up

--- src/app/configurations.helper.ts
+++ src/app/configurations.helper.ts
@@ -3,16 +3,17 @@ import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import dayjs from 'dayjs';
+import Swal from 'sweetalert2';
 import { firstValueFrom } from 'rxjs';
 import { KeycloakService, KeycloakOptions } from 'keycloak-angular';
 import type { KeycloakOnLoad } from 'keycloak-js';
+import Keycloak from 'keycloak-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigService {
   config?: SalAppConfig;
-  private readonly defaultCode = 'sal';
   originWithoutProtocol = '';
 
   constructor(
@@ -20,6 +21,7 @@ export class ConfigService {
     private message: NzMessageService,
     @Inject(PLATFORM_ID) private platformId: Object,
   ) {
+    console.log('reached here');
     if (isPlatformBrowser(this.platformId)) {
       this.originWithoutProtocol = window.location.origin.replace(/^https?:\/\/|\/$/g, '');
     }
@@ -43,27 +45,28 @@ export class ConfigService {
   async loadConfig(retries = 0): Promise<SalAppConfig | void> {
     if (!isPlatformBrowser(this.platformId)) return;
 
-    const stored = ConfigService.config;
-    if (stored?.hospital?.sub_domain === this.defaultCode) {
+    const { config: stored, isValid } = ConfigService.validConfig;
+    if (isValid && stored?.hospital?.sub_domain) {
       this.config = stored;
       return stored;
     }
+
+    const code = await ConfigService.fetchConfig();
+    if (!code) return;
 
     try {
       const response = await firstValueFrom(
         this.http.post<[SalAppConfig]>(
           'https://configuration-manager-service-dunur.ondigitalocean.app/configurations/filter',
-          { hospital_sub_domain: this.defaultCode },
+          { hospital_sub_domain: code },
         ),
       );
 
-      if (!response?.length) {
-        return;
-      }
+      if (!response?.length) return;
 
       const config = response[0];
       this.config = config;
-      this.saveConfig(config, this.defaultCode);
+      this.saveConfig(config, code);
       return config;
     } catch {
       if (retries < 3) {
@@ -71,6 +74,122 @@ export class ConfigService {
         return this.loadConfig(retries + 1);
       }
     }
+  }
+
+  static logout(keycloakInstance: Keycloak, clearConfig = false) {
+    if (clearConfig) {
+      ConfigService.clearConfig(true).then(() => {
+        keycloakInstance.logout({ redirectUri: window.location.origin });
+      });
+    } else {
+      keycloakInstance.logout({ redirectUri: window.location.origin });
+    }
+  }
+
+  static async showSnacks(message: NzMessageService) {
+    message.loading(`Loading ${ConfigService.validConfig.config?.hospital?.name}`, {
+      nzDuration: 5000,
+    });
+    ConfigService.loadAndConfigureConfig();
+  }
+
+  static loadAndConfigureConfig() {
+    ConfigService.fetchConfig().then((code) => {
+      if (code) {
+        fetch(
+          'https://configuration-manager-service-dunur.ondigitalocean.app/configurations/filter',
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ hospital_sub_domain: code }),
+          },
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            if (data[0]?.hospital?.sub_domain) {
+              ConfigService.setConfig(data[0]);
+            }
+          });
+      }
+    });
+  }
+
+  static fetchConfig = async (): Promise<string | null> => {
+    const result = await Swal.fire({
+      text: 'Please enter hospital code',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off',
+      },
+      inputAutoFocus: true,
+      inputValidator: (value) => {
+        if (!value) {
+          return 'Please enter hospital code';
+        }
+        return false;
+      },
+    });
+
+    if (!result.value) {
+      alert('Must enter a hospital code');
+      return null;
+    }
+
+    return result.value as string;
+  };
+
+  static setConfig(config: SalAppConfig) {
+    localStorage.setItem('sal-config$', JSON.stringify(config));
+    localStorage.setItem(
+      'sal-config-expiry$',
+      new Date(dayjs().add(10, 'minutes').toDate()).getTime().toString(),
+    );
+    localStorage.setItem(
+      'sal-config-origin$',
+      window.location.origin.replace(/^https?:\/\/|\/$/g, ''),
+    );
+    localStorage.setItem('sal-config-code$', config.hospital.sub_domain);
+
+    console.log('CONFIG', this.config);
+    window.location.reload();
+  }
+
+  static clearConfig(force = false) {
+    return Swal.fire({
+      text: `You are currently connected to ${
+        ConfigService.validConfig.config?.hospital?.name ?? 'an organization'
+      }. Do you want to keep the settings?`,
+      icon: 'warning',
+      showDenyButton: true,
+      denyButtonText: 'No',
+      confirmButtonText: 'Yes',
+      showCancelButton: false,
+      confirmButtonColor: '#1e88e5',
+    }).then((res) => {
+      if (res.isDenied) {
+        localStorage.removeItem('sal-config$');
+        localStorage.removeItem('sal-config-expiry$');
+        localStorage.removeItem('sal-config-origin$');
+        localStorage.removeItem('sal-config-code$');
+      }
+    });
+  }
+
+  static get validConfig() {
+    const storedConfig: SalAppConfig = JSON.parse(localStorage.getItem('sal-config$') || '{}');
+    const storedConfigExpiry = Number(localStorage.getItem('sal-config-expiry$'));
+    const storedConfigOrigin = localStorage.getItem('sal-config-origin$');
+    const storedConfigCode = localStorage.getItem('sal-config-code$');
+
+    return {
+      config: storedConfig,
+      isValid:
+        storedConfigOrigin === window.location.origin.replace(/^https?:\/\/|\/$/g, '') &&
+        storedConfig &&
+        storedConfigExpiry &&
+        dayjs().isBefore(storedConfigExpiry) &&
+        storedConfig?.hospital?.sub_domain === storedConfigCode,
+    };
   }
 }
 
@@ -83,12 +202,39 @@ export interface SalAppConfig {
   };
   frontend_config: {
     title: string;
+    care_api: string;
+    hmo_api: string;
+    production: boolean;
+    url: string;
     records_api: string;
+    clinicals_api: string;
+    clinicals_engine_api: string;
+    medical_history_v2_api: string;
+    notification_api: string;
+    NOTIFICATION_SOCKET_api_V2: string;
+    medical_history_api: string;
+    medical_history_golang_api: string;
+    pharmacy_api: string;
+    old_pharmacy_api: string;
+    procedure_api: string;
+    staff_api: string;
+    services_api: string;
+    finance_api: string;
+    lab_api: string;
+    admission_api: string;
+    mobile_api: string;
+    upload_file_api: string;
+    store_api: string;
+    user_guide_api: string;
+    emailUrl: string;
+    messaging_api: string;
+    socket_api: string;
     keycloak_base_url: string;
     keycloak_realm: string;
     keycloak_client_id: string;
     [key: string]: any;
   };
+  messaging_mode: 'TWILIO' | 'INVIDEO';
   keycloakConfig?: {
     clientId: string;
     realm: string;
@@ -98,6 +244,10 @@ export interface SalAppConfig {
     onLoad: KeycloakOnLoad;
     silentCheckSsoRedirectUri: string;
   };
+  ZEGO_APP_ID: number;
+  tiny_mc: {
+    api_key: string;
+  };
 }
 
 export function initializer(
@@ -105,16 +255,12 @@ export function initializer(
   configService: ConfigService,
 ): () => Promise<boolean> {
   return async (): Promise<boolean> => {
-    if (typeof window === 'undefined') {
-      return true;
-    }
+    if (typeof window === 'undefined') return true;
 
     try {
       await configService.loadConfig();
 
-      if (!configService.config) {
-        return true;
-      }
+      if (!configService.config) return true;
 
       const options: KeycloakOptions = {
         config: {
@@ -131,8 +277,7 @@ export function initializer(
         bearerExcludedUrls: ['/assets', '/configurations/filter'],
       };
 
-      const initialized = await keycloak.init(options);
-      return initialized;
+      return await keycloak.init(options);
     } catch {
       return true;
     }
